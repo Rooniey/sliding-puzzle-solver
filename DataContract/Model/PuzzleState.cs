66aaa3d569aab010ac6c780a7b6920cee298aa64
kpilcicki <36018832+kpilcicki@users.noreply.github.com>
2018-11-21 20:04:40 +0100

    SOME WORK

--- DataContract/Model/PuzzleState.cs
+++ DataContract/Model/PuzzleState.cs
@@ -1,9 +1,60 @@
-﻿namespace DataContract.Model
+﻿using System.Linq;
+
+namespace DataContract.Model
 {
     public class PuzzleState
     {
         public byte DimensionX { get; set; }
         public byte DimensionY { get; set; }
         public byte[] State { get; set; }
+
+        public bool IsSolved()
+        {
+            for(int i = 0; i < State.Length; i++)
+            {
+                if (State[i] != (i + 1) % State.Length) return false;
+            }
+            return true;
+        }
+
+        public string PossibleMoves(int zeroIndex)
+        {
+            if (zeroIndex == 0) return "RD";
+            if (zeroIndex == DimensionX - 1) return "LD";
+            if (zeroIndex == DimensionX * (DimensionY - 1) - 1) return "UR";
+            if (zeroIndex == State.Length - 1) return "LU";
+            
+            if (zeroIndex < DimensionX && zeroIndex > 0) return "LRD";
+            if (zeroIndex % DimensionX == 0 && zeroIndex != 0 && zeroIndex != DimensionX * (DimensionY - 1) - 1) return "URD";
+            var revLen = State.Length - zeroIndex;
+            if (revLen % DimensionX == 0 && revLen != 0 && revLen != DimensionX * (DimensionY - 1) - 1) return "LUD";
+            if (revLen < DimensionX && revLen > 0) return "LUR";
+
+            return "LURD";
+        }
+
+        public void MoveLeft(int zeroIndex)
+        {
+            State[zeroIndex] = State[zeroIndex - 1];
+            State[zeroIndex - 1] = 0;
+        }
+
+        public void MoveRight(int zeroIndex)
+        {
+            State[zeroIndex] = State[zeroIndex + 1];
+            State[zeroIndex + 1] = 0;
+        }
+
+        public void MoveUp(int zeroIndex)
+        {
+            State[zeroIndex] = State[zeroIndex - DimensionX];
+            State[zeroIndex - DimensionX] = 0;
+        }
+
+        public void MoveDown(int zeroIndex)
+        {
+            State[zeroIndex] = State[zeroIndex + DimensionX];
+            State[zeroIndex + DimensionX] = 0;
+        }
     }
 }
--- SiseAssignment/Class1.cs
+++ SiseAssignment/Class1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataContract.Model;
+using DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,40 @@ namespace SiseAssignment
 {
     public class Class1
     {
+        public static int MAX_DEPTH = 25;
+
+        public void SolveBFS(string strategy, string inputFilePath, string solutionFilePath, string statisticsFilePath)
+        {
+            PuzzleState initialState = InitialStateReader.ReadStateFromFile(inputFilePath);
+
+            var index = 0;
+            foreach(var item in initialState.State)
+            {
+                if (item == 0) break;
+                index++;
+            }
+
+            while(!initialState.IsSolved())
+            {
+                foreach(char item in strategy.Where(e => initialState.PossibleMoves(index).Contains(e)))
+                {
+                    switch(item)
+                    {
+                        case 'L':
+                            initialState.MoveLeft(index);
+                            break;
+                        case 'U':
+                            initialState.MoveUp(index);
+                            break;
+                        case 'R':
+                            initialState.MoveRight(index);
+                            break;
+                        case 'D':
+                            initialState.MoveDown(index);
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
